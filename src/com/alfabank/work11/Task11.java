79a63d98454f439bf4d9e36eceef3edc2fe956b3
Kordyukov <mrkordyukov@gmail.com>
2021-06-02 19:36:03 +0300

    Initial commit

---
+++ src/com/alfabank/work11/Task11.java
@@ -0,0 +1,11 @@
+package com.alfabank.work11;
+
+public class Task11 {
+
+    public static void main(String[] args) {
+//        System.out.println("Hello Student!");
+	// write your code here
+        System.err.print("Mystang ");
+        System.out.println("aaaa");
+    }
+}
---
+++ src/com/alfabank/work12/Task12.java
@@ -0,0 +1,12 @@
+package com.alfabank.work12;
+
+public class Task12 {
+    public static void main(String[] args) {
+        System.out.println(" ***       *    *         *   *   ");
+        System.out.println("    *    *  *    *       *   * *  ");
+        System.out.println("    *    *   *    *     *   *  *  ");
+        System.out.println("    *   *    *     *   *   *   *  ");
+        System.out.println("*   *   *****       * *   ******  ");
+        System.out.println("*** *   *    *       *   *      * ");
+    }
+}
